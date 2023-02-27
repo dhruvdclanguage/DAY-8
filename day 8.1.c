545acc613105f613e747c2b2df470491f1ed33e8
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-02-27 12:34:57 +0530

    Add files via upload

---
+++ day 8.1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main () {
+	
+	int a , b ;
+	
+	printf("write a number A :");
+	scanf("%d",&a);
+	
+	printf("write a number B :");
+	scanf("%d",&b);
+	
+	if(a<b) {
+		
+		printf("min number is A");
+	}
+     else{
+     	printf("main number is B");
+	 }	
+	
+	
+	
+	return 0;
+	
+}
